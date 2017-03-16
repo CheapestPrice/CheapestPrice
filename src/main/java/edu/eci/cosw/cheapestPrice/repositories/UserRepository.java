52a43f6465e0a4da9359d649b7c6224aaaaea2c7
German Lopez <germaniuslp@hotmail.com>
2017-03-16 17:09:33 -0500

    JPARepository

---
+++ src/main/java/edu/eci/cosw/cheapestPrice/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package edu.eci.cosw.cheapestPrice.repositories;
+
+import edu.eci.cosw.cheapestPrice.entities.ListaDeMercado;
+import edu.eci.cosw.cheapestPrice.entities.Usuario;
+import edu.eci.cosw.cheapestPrice.exception.CheapestPriceException;
+import edu.eci.cosw.cheapestPrice.persistence.UserPersistence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 2105403 on 3/16/17.
+ */
+public interface UserRepository extends JpaRepository<Usuario,Integer> {
+
+}
