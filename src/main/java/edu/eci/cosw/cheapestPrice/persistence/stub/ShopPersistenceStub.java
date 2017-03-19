245f423e1c91d1571f34a12477be9fc788366e77
Daniela <daniela.sepulveda@mail.escuelaing.edu.co>
2017-03-18 22:22:11 -0500

    Shop persistence

--- src/main/java/edu/eci/cosw/cheapestPrice/persistence/stub/ShopPersistenceStub.java
+++ src/main/java/edu/eci/cosw/cheapestPrice/persistence/stub/ShopPersistenceStub.java
@@ -1,14 +1,18 @@
 package edu.eci.cosw.cheapestPrice.persistence.stub;
 
-import edu.eci.cosw.cheapestPrice.entities.Tienda;
+import com.mysql.jdbc.Blob;
+import edu.eci.cosw.cheapestPrice.entities.*;
+import edu.eci.cosw.cheapestPrice.exception.CheapestPriceException;
 import edu.eci.cosw.cheapestPrice.persistence.ShopPersistence;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- * Created by Paula on 21/02/2017.
+ * Created by Daniela on 21/02/2017.
  */
 @Service
 public class ShopPersistenceStub implements ShopPersistence {
@@ -17,18 +21,59 @@ public class ShopPersistenceStub implements ShopPersistence {
 
     public ShopPersistenceStub(){ tiendas=new HashMap<>();}
 
-
     @Override
     public void addTienda(Tienda tienda) {
-        System.out.println("Si entro!: "+tienda.tendero.getCorreo());
         tiendas.put(tienda.tendero.getCorreo(),tienda);
+    }
 
+    @Override
+    public Item loadItem(long id) throws CheapestPriceException {
+        return null;
+    }
+
+    @Override
+    public List<Item> loadItems() {
+        return null;
+    }
+
+    @Override
+    public void addProduct(Producto producto) throws CheapestPriceException {
 
     }
 
     @Override
-    public Tienda getTienda(String nickname) {
-        return tiendas.get(nickname);
+    public void deleteProduct(long idproducto) throws CheapestPriceException {
+
+    }
+
+    @Override
+    public void modifyProduct(long idproducto, Producto producto) throws CheapestPriceException {
+
+    }
+
+    @Override
+    public void modifyHorary(String dia, Horario horario) throws CheapestPriceException {
+
+    }
+
+    @Override
+    public void modifyTelephone(String telefono) throws CheapestPriceException {
+
+    }
+
+    @Override
+    public boolean isOpen(Timestamp fecha) throws CheapestPriceException {
+        return false;
+    }
+
+    @Override
+    public void addOpinion(Opinion opinion) throws CheapestPriceException {
+
+    }
+
+    @Override
+    public void modifyLogo(Blob logo) throws CheapestPriceException {
+
     }
 
     public Map<String, Tienda> getTiendas() {
@@ -38,4 +83,5 @@ public class ShopPersistenceStub implements ShopPersistence {
     public void setTiendas(Map<String, Tienda> tiendas) {
         this.tiendas = tiendas;
     }
+
 }
