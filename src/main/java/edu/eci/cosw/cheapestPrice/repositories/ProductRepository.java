c768307f36ef289474635a3f961920576f3263c8
Hugo Alvarez <hugoferneya@hotmail.com>
2017-03-20 16:08:28 -0500

    repositorio producto elementos añadidos

--- src/main/java/edu/eci/cosw/cheapestPrice/repositories/ProductRepository.java
+++ src/main/java/edu/eci/cosw/cheapestPrice/repositories/ProductRepository.java
@@ -17,10 +17,10 @@ public interface ProductRepository extends JpaRepository<Producto, Long> {
     @Query("select p from Producto p")
     public List<Producto> loadAllProductos();
     @Query("update Producto p set p.nombre=:producto.nombre where p.id=:producto.id")
-    public List<Producto> updateByName();
+    public List<Producto> updateByName(@Param("producto") Producto producto);
     @Query("update Producto p set p.marca=:producto.marca where p.id=:producto.id")
-    public List<Producto> updateByMarca();
+    public List<Producto> updateByMarca(@Param("producto") Producto producto);
     @Query("update Producto p set p.categoria=:producto.categoria where p.id=:producto.id")
     public List<Producto> updateByCategoria(@Param("producto") Producto producto);
-    
+
 }
