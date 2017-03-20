7c2cd7b7aab6134bdec1ab64faf38850789500ba
Hugo Alvarez <hugoferneya@hotmail.com>
2017-03-20 15:55:33 -0500

    repositorio producto

---
+++ src/main/java/edu/eci/cosw/cheapestPrice/repositories/ProductRepository.java
@@ -0,0 +1,26 @@
+package edu.eci.cosw.cheapestPrice.repositories;
+
+
+import edu.eci.cosw.cheapestPrice.entities.Producto;
+import edu.eci.cosw.cheapestPrice.entities.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * Created by masterhugo on 3/20/17.
+ */
+public interface ProductRepository extends JpaRepository<Producto, Long> {
+    @Query("select p from Producto p")
+    public List<Producto> loadAllProductos();
+    @Query("update Producto p set p.nombre=:producto.nombre where p.id=:producto.id")
+    public List<Producto> updateByName();
+    @Query("update Producto p set p.marca=:producto.marca where p.id=:producto.id")
+    public List<Producto> updateByMarca();
+    @Query("update Producto p set p.categoria=:producto.categoria where p.id=:producto.id")
+    public List<Producto> updateByCategoria(@Param("producto") Producto producto);
+    
+}
