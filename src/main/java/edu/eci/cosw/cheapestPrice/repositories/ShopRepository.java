606a6b79c8aa862f6d7830c3a09ff570397fad67
German Lopez <germaniuslp@hotmail.com>
2017-03-20 16:44:42 -0500

    Arreglando lloraderas

--- src/main/java/edu/eci/cosw/cheapestPrice/repositories/ShopRepository.java
+++ src/main/java/edu/eci/cosw/cheapestPrice/repositories/ShopRepository.java
@@ -14,20 +14,19 @@ import java.util.List;
  * Created by Daniela on 14/03/17.
  */
 public interface ShopRepository extends JpaRepository<Tienda,TiendaId>{
-
-    @Query("select t.items from Tienda t where t.id.nit=:idtienda.nit and t.id.x=:idtienda.x and t.id.y=:idtienda.y")
+    @Query("select t.items from Tienda t where t.id.nit=:#{#idtienda.nit} and t.id.x=:#{#idtienda.x} and t.id.y=:#{#idtienda.y}")
     public List<Item> loadItems(@Param("idtienda") TiendaId idtienda);
 
-    @Query("select i from Item i where i.tienda.id.nit=:idtienda.nit and i.tienda.id.x=:idtienda.x and i.tienda.id.y=:idtienda.y and i.producto.id=:idporducto")
+    @Query("select i from Item i where i.tienda.id.nit=:#{#idtienda.nit} and i.tienda.id.x=:#{#idtienda.x} and i.tienda.id.y=:#{#idtienda.y} and i.producto.id=:idproducto")
     public Item loadItem(@Param("idtienda") TiendaId idtienda,@Param("idproducto") long idprodcuto);
 
-    @Query("")
-    public void updateShop(@Param("idtienda") TiendaId idtienda, @Param("tienda") Tienda tienda);
+    //@Query("")
+    //public void updateShop(@Param("idtienda") TiendaId idtienda, @Param("tienda") Tienda tienda);
 
-    @Query("update Tienda t set t.logo=:logo where t.id.nit=:idtienda.nit and t.id.x=:idtienda.x and t.id.y=:idtienda.y")
+    @Query("update Tienda t set t.logo=:logo where t.id.nit=:#{#idtienda.nit} and t.id.x=:#{#idtienda.x} and t.id.y=:#{#idtienda.y}")
     public void modifyLogo(@Param("idtienda") TiendaId id, @Param("logo") Blob logo);
 
-    @Query("update Tienda t set t.telefono=:telefono where t.id.nit=:idtienda.nit and t.id.x=:idtienda.x and t.id.y=:idtienda.y")
+    @Query("update Tienda t set t.telefono=:telefono where t.id.nit=:#{#idtienda.nit} and t.id.x=:#{#idtienda.x} and t.id.y=:#{#idtienda.y}")
     public void modifyTelephone(@Param("idtienda") TiendaId id, @Param("telefono") String telefono);
 
 }
