2d74abcbc9977ec028a37f008acd879fe1361d9f
Julian <judadese@gmail.com>
2017-03-27 14:29:32 -0500

    hay ruta :D

--- src/main/resources/static/app/map/map.js
+++ src/main/resources/static/app/map/map.js
@@ -2,20 +2,15 @@
 
 angular.module('myApp.viewMap', ['ngRoute'])
 
-    .config(['$routeProvider', '$sceDelegateProvider', function ($routeProvider, $sceDelegateProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/viewMap', {
             templateUrl: 'map/map.html',
             controller: 'ViewMapCtrl'
         });
-        //$sceDelegateProvider.resourceUrlWhitelist(['self','https://maps.googleapis.com/maps/api/directions/**'])
     }])
     .controller('ViewMapCtrl', ['uiGmapGoogleMapApi', '$scope', '$rootScope', '$location', '$http','$document', function (uiGmapGoogleMapApi, $scope, $rootScope, $location, $http,$document) {
         console.log($rootScope.x + " " + $rootScope.y);
-        /*window.map = new google.maps.Map(document.getElementById('map'), {
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false
-        });*/
-        $scope.map = {center: {latitude: $rootScope.x, longitude: $rootScope.y}, zoom: 15};
+        $scope.map = {center: {latitude: $rootScope.x, longitude: $rootScope.y}, zoom: 15,control : {}};
         $scope.curr = {
             id: 'source',
             center: {
@@ -23,16 +18,7 @@ angular.module('myApp.viewMap', ['ngRoute'])
                 longitude : $rootScope.y
             }
         };
-        $scope.points=[];
-        $scope.markers=[
-            {
-                id: 'source',
-                center: {
-                    latitude : $rootScope.x,
-                    longitude : $rootScope.y
-                }
-            }
-        ];
+        $scope.markers=[{id: 'source',center: {latitude : $rootScope.x,longitude : $rootScope.y}}];
         var places=[
             {
                 x: 4.750667,
@@ -43,40 +29,37 @@ angular.module('myApp.viewMap', ['ngRoute'])
                 y: -74.065019
             }
         ];
+        $scope.request={
+            origin: {
+                    lat : $rootScope.x,
+                    lng : $rootScope.y
+                },
+            destination:{
+                    lat : $rootScope.x,
+                    lng : $rootScope.y
+                },
+            travelMode : google.maps.TravelMode.WALKING,
+            waypoints:[],
+            optimizeWaypoints: true
+        };
         places.forEach(function(item,index){
-            $scope.markers.push({
-                id: index+"",
-                center: {
-                    latitude: item.x,
-                    longitude: item.y
-                }
-
-            });
-            $scope.points.push({
-                latitude: item.x,
-                longitude: item.y
+            $scope.markers.push({id: index+"",center: {latitude: item.x,longitude: item.y}});
+            $scope.request.waypoints.push({
+                location:{
+                    lat: item.x,
+                    lng: item.y
+                },
+                stopover:true
             });
         });
-        /*var directionsDisplay = new google.maps.DirectionsRenderer();
+        var directionsDisplay = new google.maps.DirectionsRenderer();
         var directionsService = new google.maps.DirectionsService();
-        var geocoder = new google.maps.Geocoder();
-        var bounds = new google.maps.LatLngBounds();*/
-        //bounds.extend($scope.source.center);
-
-        /*var apiKey="AIzaSyBYRVtSxaERYgZoESFx_4pExGXMbHHU8F8";
-         var url="https://maps.googleapis.com/maps/api/directions/json?origin=4.7809235999999995,-74.04762629999999&destination=4.7809235999999995,-74.04762629999999&key=AIzaSyBYRVtSxaERYgZoESFx_4pExGXMbHHU8F8";
-         $.ajax({
-         url:url,
-         type: 'GET',
-         dataType: 'jsonp',
-         cache: false
-         })
-         .done(function(data){
-         console.log("it worked");
-         console.log(data);
-         });
-         var work=function(data){
-         console.log(data);
-         }*/
-        //console.log(googleAns);
+        directionsService.route($scope.request, function (response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap($scope.map.control.getGMap());
+            } else {
+                alert('Google route unsuccesfull!');
+            }
+        });     
     }])
