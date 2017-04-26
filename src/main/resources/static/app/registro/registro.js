a019f73f1f499fd5f26b37e1269a8b6d020966dd
Hugo Alvarez <hugoferneya@hotmail.com>
2017-04-26 10:02:38 -0500

    Ya casi registrar tienda

--- src/main/resources/static/app/registro/registro.js
+++ src/main/resources/static/app/registro/registro.js
@@ -9,9 +9,10 @@ angular.module('myApp.registro', ['ngRoute'])
   });
 }])
 
-.controller('registro', ['bcrypt','$scope', 'registroU','registroC','registroT', function (bcrypt,$scope, registroU, registroC, registroT)  {
+.controller('registro', ['bcrypt','$scope', '$rootScope', 'registroU','registroC','registroT','$mdDialog', function (bcrypt,$scope,$rootScope, registroU, registroC, registroT,$mdDialog)  {
 
     $scope.validada=false;
+    $scope.validada2=false;
 
     $scope.tienda=function(){
         if($scope.validada) $scope.validada = false;
@@ -32,26 +33,11 @@ angular.module('myApp.registro', ['ngRoute'])
         }
     }
 
-    $scope.registroTienda = function(){
+    $scope.registroTienda = function(ev){
         if($scope.validada){
             if(($scope.password==$scope.passwordConf)&& ($scope.nitTienda!=null)){
-                var latitud;
-                var longitud;
-                var geocoder = new google.maps.Geocoder();
-                var address = $scope.dirTienda+",Bogota";
-                geocoder.geocode( { 'address': address}, function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        latitud = results[0].geometry.location.lat();
-                        longitud = results[0].geometry.location.lng();
-                        console.log('La longitud es: ' + longitud + ', la latitud es: ' + latitud);
-                        var tiendaId={"nit":$scope.nitTienda,"latitud":latitud,"longitud":longitud};
-                        var tendero={"nombre":$scope.nombre,"email":$scope.email,"nickname":$scope.nickname};
-                        var cuenta={"email":$scope.email, "hash:" :$scope.password, "rol":'Tendero'}
-                        var tienda={"direccion":$scope.dirTienda,"id":tiendaId,"nombre":$scope.nomTienda,"telefono":$scope.telTienda,"tendero":tendero};
-                        registroT.save(tienda).$promise.then(registroC.save(cuenta));
+                $scope.showAdvanced(ev);
 
-                    }
-                });
             }
             else{
                 alert("Llena los campos correctamente");
@@ -65,7 +51,7 @@ angular.module('myApp.registro', ['ngRoute'])
                 cuenta.email = $scope.email;
                 cuenta.hash = $scope.password;
                 cuenta.rol = 'Usuario';
-                registroU.save(usuario).$promise.then(registroC.save(cuenta));
+                //registroU.save(usuario).$promise.then(registroC.save(cuenta));
 
 
             }
@@ -75,6 +61,65 @@ angular.module('myApp.registro', ['ngRoute'])
         }
 
     };
+    $scope.showAdvanced = function(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'registro/mapaConfirm.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true,
+          onComplete: function(){
+                $scope.mapi();
+             }
+        })
+            .then(function(answer) {
+              $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+              $scope.status = 'You cancelled the dialog.';
+            });
+      };
+      $scope.mapi = function(){
+        console.log($rootScope.x+", "+$rootScope.y);
+        var latitud;
+        var longitud;
+        var latlon = "";
+        var geocoder = new google.maps.Geocoder();
+        var address = $scope.dirTienda+",Bogota";
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                console.log(results);
+                latitud = results[0].geometry.location.lat();
+                longitud = results[0].geometry.location.lng();
+                console.log('La longitud es: ' + longitud + ', la latitud es: ' + latitud);
+                latlon = latitud + "," + longitud;
+                var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+                    +latlon+"&zoom=17&size=400x300&markers=color:blue|label:H|"
+                    +latlon+"&sensor=true&key=AIzaSyAHWCLBHGvJpCZkc82kL9w9FykQB6Zy3pM";
+                document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+                //$scope.validada2=true;
+                var tiendaId={"nit":$scope.nitTienda,"latitud":latitud,"longitud":longitud};
+                var tendero={"nombre":$scope.nombre,"email":$scope.email,"nickname":$scope.nickname};
+                var cuenta={"email":$scope.email, "hash:" :$scope.password, "rol":'Tendero'}
+                var tienda={"direccion":$scope.dirTienda,"id":tiendaId,"nombre":$scope.nomTienda,"telefono":$scope.telTienda,"tendero":tendero};
+                //registroT.save(tienda).$promise.then(registroC.save(cuenta));
+                    }
+                        });
+      }
+      function DialogController($scope, $mdDialog, $rootScope) {
+
+              $scope.hide = function() {
+                $mdDialog.hide();
+              };
+
+              $scope.cancel = function() {
+                $mdDialog.cancel();
+              };
+
+              $scope.answer = function() {
+                $mdDialog.hide();
+              };
+        }
 
 
 
