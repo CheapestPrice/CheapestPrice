f137c01de48c08e881add40aa111421bbd9da59d
Julián David Devia Serna <judadese@gmail.com>
2017-05-03 09:16:06 -0500

    sql actualizado de la rama inestable

--- insert.sql
+++ insert.sql
@@ -15,25 +15,25 @@ INSERT INTO `TIENDAS` (`direccion`,`x`,`y`,`nombre`,`nit`,`telefono`,`disponible
 	('La Caro #197-2 a 197-54',4.775933, -74.041923,'Donde Carlos','1295567-2','1234567',true,null);
 
 LOCK TABLES `TENDEROS` WRITE;
-INSERT INTO `TENDEROS` (`USUARIOS_correo`,`nombre`,`TIENDAS_x`,`TIENDAS_y`,`TIENDAS_nit`) VALUES
-	('tendero@tendero.com','tendero1',4.7649271,-74.0476042,'1234567-2'),
-	('tendero2@tendero.com','tendero2',4.775933, -74.041923,'1295567-2'),
-	('tendero3@tendero.com','tendero3',4.788479, -74.045011,'1232567-2');
+INSERT INTO `TENDEROS` (`USUARIOS_id`,`nombre`,`TIENDAS_id`) VALUES
+	(3,'tendero1',1),
+	(5,'tendero2',3),
+	(7,'tendero3',2);
 
 LOCK TABLES `HORARIOS` WRITE;
-INSERT INTO `HORARIOS` (`horaInicio`,`horaFin`,`minutosInicio`,`minutosFin`,`Tiendas_x`,`Tiendas_y`,`Tiendas_nit`,`dia`) VALUES
-	(7,19,0,0,4.7649271,-74.0476042,'1234567-2','Lunes'),
-	(7,19,0,0,4.7649271,-74.0476042,'1234567-2','Jueves'),
-	(7,19,0,0,4.775933, -74.041923,'1295567-2','Lunes'),
-	(7,19,0,0,4.775933, -74.041923,'1295567-2','Martes'),
-	(7,19,0,0,4.775933, -74.041923,'1295567-2','Miércoles'),
-	(7,19,0,0,4.775933, -74.041923,'1295567-2','Jueves'),
-	(7,19,0,0,4.775933, -74.041923,'1295567-2','Viernes'),
-	(7,19,0,0,4.788479, -74.045011,'1232567-2','Lunes'),
-	(7,19,0,0,4.788479, -74.045011,'1232567-2','Martes'),
-	(7,19,0,0,4.788479, -74.045011,'1232567-2','Miércoles'),
-	(7,19,0,0,4.788479, -74.045011,'1232567-2','Jueves'),
-	(7,19,0,0,4.788479, -74.045011,'1232567-2','Viernes');
+INSERT INTO `HORARIOS` (`horaInicio`,`horaFin`,`minutosInicio`,`minutosFin`,`TIENDAS_id`,`dia`) VALUES
+	(7,19,0,0,1,'Lunes'),
+	(7,19,0,0,1,'Jueves'),
+	(7,19,0,0,3,'Lunes'),
+	(7,19,0,0,3,'Martes'),
+	(7,19,0,0,3,'Miércoles'),
+	(7,19,0,0,3,'Jueves'),
+	(7,19,0,0,3,'Viernes'),
+	(7,19,0,0,2,'Lunes'),
+	(7,19,0,0,2,'Martes'),
+	(7,19,0,0,2,'Miércoles'),
+	(7,19,0,0,2,'Jueves'),
+	(7,19,0,0,2,'Viernes');
 
 LOCK TABLES `PRODUCTOS` WRITE;
 INSERT INTO `PRODUCTOS` (`nombre`,`id`,`marca`,`imagen`,`categoria`) VALUES
@@ -48,60 +48,60 @@ INSERT INTO `PRODUCTOS` (`nombre`,`id`,`marca`,`imagen`,`categoria`) VALUES
 	('Queso de Untar',9,'Colanta',null,'Lacteos');
 
 LOCK TABLES `ITEMS` WRITE;
-INSERT INTO `ITEMS`    (`PRODUCTOS_id`,`TIENDAS_x`,`TIENDAS_y`,`TIENDAS_nit`,`precio`) VALUES
-	(1,4.7649271,-74.0476042,'1234567-2',3000),
-	(2,4.7649271,-74.0476042,'1234567-2',2500),
-	(3,4.7649271,-74.0476042,'1234567-2',4000),
-	(4,4.7649271,-74.0476042,'1234567-2',500),
-	(1,4.775933, -74.041923,'1295567-2',3000),
-	(2,4.775933, -74.041923,'1295567-2',2500),
-	(3,4.775933, -74.041923,'1295567-2',4000),
-	(4,4.775933, -74.041923,'1295567-2',500),
-	(5,4.775933, -74.041923,'1295567-2',1000),
-	(6,4.775933, -74.041923,'1295567-2',1200),
-	(7,4.775933, -74.041923,'1295567-2',4000),
-	(8,4.775933, -74.041923,'1295567-2',2000),
-	(1,4.788479, -74.045011,'1232567-2',2500),
-	(2,4.788479, -74.045011,'1232567-2',3000),
-	(3,4.788479, -74.045011,'1232567-2',3500),
-	(4,4.788479, -74.045011,'1232567-2',1000),
-	(5,4.788479, -74.045011,'1232567-2',1500),
-	(6,4.788479, -74.045011,'1232567-2',1200),
-	(7,4.788479, -74.045011,'1232567-2',4100),
-	(8,4.788479, -74.045011,'1232567-2',2900);
+INSERT INTO `ITEMS`    (`PRODUCTOS_id`,`TIENDAS_id`,`precio`) VALUES
+	(1,1,3000),
+	(2,1,2500),
+	(3,1,4000),
+	(4,1,500),
+	(1,3,3000),
+	(2,3,2500),
+	(3,3,4000),
+	(4,3,500),
+	(5,3,1000),
+	(6,3,1200),
+	(7,3,4000),
+	(8,3,2000),
+	(1,2,2500),
+	(2,2,3000),
+	(3,2,3500),
+	(4,2,1000),
+	(5,2,1500),
+	(6,2,1200),
+	(7,2,4100),
+	(8,2,2900);
 
 LOCK TABLES `LISTAS_MERCADOS` WRITE;
-INSERT INTO `LISTAS_MERCADOS` (`nombre`,`fechaCreacion`,`realizado`,`USUARIOS_correo`) VALUES
-	('ListaMercado1','2017-01-01 00:00:01',false,'prueba@prueba.com'),
-    ('ListaMercado2','2017-01-10 00:00:01',false,'admin@cheapestprice.com'),
-	('ListaMercado','2017-03-28 00:00:01',false,'admin@cheapestprice.com'),
-	('ListaMercado','2017-03-28 00:00:01',false,'prueba@prueba.com'),
-	('ListaMercado','2017-03-28 00:00:01',false,'prueba2@prueba.com');
+INSERT INTO `LISTAS_MERCADOS` (`nombre`,`fechaCreacion`,`realizado`,`USUARIOS_id`) VALUES
+	('ListaMercado1','2017-01-01 00:00:01',false,2),
+   	('ListaMercado2','2017-01-10 00:00:01',false,1),
+	('ListaMercado','2017-03-28 00:00:01',false,1),
+	('ListaMercado','2017-03-28 00:00:01',false,2),
+	('ListaMercado','2017-03-28 00:00:01',false,4);
 
 LOCK TABLES `ITEMS_LISTA` WRITE;
-INSERT INTO `ITEMS_LISTA` (`comprado`,`favorito`,`ITEMS_PRODUCTOS_id`,`ITEMS_TIENDAS_x`,`ITEMS_TIENDAS_y`,`ITEMS_TIENDAS_nit`,`LISTAS_MERCADOS_nombre`,`LISTAS_MERCADOS_USUARIOS_correo`) 	VALUES
-	(false,false,1,4.7649271,-74.0476042,'1234567-2','ListaMercado1','prueba@prueba.com'),
-	(false,false,3,4.7649271,-74.0476042,'1234567-2','ListaMercado1','prueba@prueba.com'),
-	(false,false,4,4.7649271,-74.0476042,'1234567-2','ListaMercado1','prueba@prueba.com'),
-	(false,false,2,4.7649271,-74.0476042,'1234567-2','ListaMercado2','admin@cheapestprice.com'),
-	(false,false,3,4.7649271,-74.0476042,'1234567-2','ListaMercado2','admin@cheapestprice.com'),
-	(false,false,4,4.7649271,-74.0476042,'1234567-2','ListaMercado2','admin@cheapestprice.com'),
-	(false,false,1,4.788479, -74.045011,'1232567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,2,4.775933, -74.041923,'1295567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,3,4.788479, -74.045011,'1232567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,4,4.775933, -74.041923,'1295567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,5,4.775933, -74.041923,'1295567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,6,4.788479, -74.045011,'1232567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,7,4.775933, -74.041923,'1295567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,8,4.775933, -74.041923,'1295567-2','ListaMercado','admin@cheapestprice.com'),
-	(false,false,1,4.788479, -74.045011,'1232567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,2,4.775933, -74.041923,'1295567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,3,4.788479, -74.045011,'1232567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,4,4.775933, -74.041923,'1295567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,5,4.775933, -74.041923,'1295567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,6,4.788479, -74.045011,'1232567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,7,4.775933, -74.041923,'1295567-2','ListaMercado','prueba@prueba.com'),
-	(false,false,8,4.775933, -74.041923,'1295567-2','ListaMercado','prueba@prueba.com');
+INSERT INTO `ITEMS_LISTA` (`comprado`,`favorito`,`ITEMS_id`,`LISTAS_MERCADOS_id`) VALUES
+	(false,false,1,1),
+	(false,false,3,1),
+	(false,false,4,1),
+	(false,false,2,2),
+	(false,false,3,2),
+	(false,false,4,2),
+	(false,false,13,3),
+	(false,false,6,3),
+	(false,false,15,3),
+	(false,false,8,3),
+	(false,false,9,3),
+	(false,false,18,3),
+	(false,false,11,3),
+	(false,false,12,3),
+	(false,false,13,4),
+	(false,false,6,4),
+	(false,false,15,4),
+	(false,false,8,4),
+	(false,false,9,4),
+	(false,false,18,4),
+	(false,false,11,4),
+	(false,false,12,4);
 
 /*prueba
 tendero
@@ -111,11 +111,11 @@ tendero
 prueba
 tendero*/
 LOCK TABLES `USUARIOS_AUTENTICACION` WRITE;
-INSERT INTO `USUARIOS_AUTENTICACION` (`USUARIOS_correo`,`hash`,`habilitado`,`rol`) VALUES
-	('prueba@prueba.com','$2a$06$xP01EJ/jiHbM76ydX52M.uYQX4GTqjvrAJyBkkrzU8Y.uUAx7hmQK',true,'Usuario'),
-	('tendero@tendero.com','$2a$06$dQSpBh.CyVqRIt8VJQiniOoJwfA2lopdKC8vt4CRYKGYFwRwqo02y',true,'Tendero'),
-	('admin@cheapestprice.com','$2a$04$9bTPMRnWTVwi.xUjK2uQn.iCZowsVnNode.hG.czbSzT3LWDNn6Gu',true,'Usuario'),
-	('prueba2@prueba.com','$2a$06$xP01EJ/jiHbM76ydX52M.uYQX4GTqjvrAJyBkkrzU8Y.uUAx7hmQK',true,'Usuario'),
-	('tendero2@tendero.com','$2a$06$dQSpBh.CyVqRIt8VJQiniOoJwfA2lopdKC8vt4CRYKGYFwRwqo02y',true,'Tendero'),
-	('prueba3@prueba.com','$2a$06$xP01EJ/jiHbM76ydX52M.uYQX4GTqjvrAJyBkkrzU8Y.uUAx7hmQK',true,'Usuario'),
-	('tendero3@tendero.com','$2a$06$dQSpBh.CyVqRIt8VJQiniOoJwfA2lopdKC8vt4CRYKGYFwRwqo02y',true,'Tendero');
+INSERT INTO `USUARIOS_AUTENTICACION` (`USUARIOS_id`,`hash`,`habilitado`,`rol`) VALUES
+	('2','711383a59fda05336fd2ccf70c8059d1523eb41a',true,'Usuario'),
+	('3','98e5e0ec81f5f7cb10f9775c359893f5734092a8',true,'Tendero'),
+	('1','d033e22ae348aeb5660fc2140aec35850c4da997',true,'Usuario'),
+	('4','711383a59fda05336fd2ccf70c8059d1523eb41a',true,'Usuario'),
+	('5','98e5e0ec81f5f7cb10f9775c359893f5734092a8',true,'Tendero'),
+	('6','711383a59fda05336fd2ccf70c8059d1523eb41a',true,'Usuario'),
+	('7','98e5e0ec81f5f7cb10f9775c359893f5734092a8',true,'Tendero');
